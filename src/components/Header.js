d3d547d020d487cd011ce959a222f9f1aae2582a
Ross Bernard Jamaca <rossbernard.jamaca@fligno.com>
2022-11-11 15:27:19 +0800

    cleanup: 🚀 clean ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 /*global AlgoSigner*/
-import React, {useEffect, useRef, useState} from 'react';
-import {Container,Row, Col, Button} from 'react-bootstrap'
+import React, { useEffect, useRef, useState } from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import logo from '../assets/images/AlgoVote.svg'
 import { CONSTANTS } from './Constants';
-import algosdk, { waitForConfirmation } from'algosdk';
+import algosdk, { waitForConfirmation } from 'algosdk';
 import MessageAlert from './Alert';
 
-export default function Header ({peraWallet}){
+export default function Header({ peraWallet }) {
   let client = new algosdk.Algodv2(CONSTANTS.algodToken, CONSTANTS.baseServer, CONSTANTS.port)
   const [alert, setAlert] = useState(false)
   const [userAccount, setUserAccount] = useState(null)
@@ -23,7 +23,8 @@ export default function Header ({peraWallet}){
   }
 
   function handleDisconnectWalletClick() {
-    peraWallet.disconnect();
+    localStorage.removeItem('walletconnect')
+    localStorage.removeItem('PeraWallet.Wallet')
     setUserAccount(null)
   }
 
@@ -41,68 +42,68 @@ export default function Header ({peraWallet}){
   }, []);
 
 
- //OPTIN
-// create unsigned transaction
-const Optin = async (sender, index) => {
-  try {
-    console.log(sender)
-    const suggestedParams = await client.getTransactionParams().do();
-    const optInTxn = algosdk.makeApplicationOptInTxn(
-      sender,
-      suggestedParams,
-      index
-    );
-    const actionTxGroup = [{ txn: optInTxn, signers: [sender] }];
+  //OPTIN
+  // create unsigned transaction
+  const Optin = async (sender, index) => {
+    try {
+      console.log(sender)
+      const suggestedParams = await client.getTransactionParams().do();
+      const optInTxn = algosdk.makeApplicationOptInTxn(
+        sender,
+        suggestedParams,
+        index
+      );
+      const actionTxGroup = [{ txn: optInTxn, signers: [sender] }];
 
-    const signedTx = await peraWallet.signTransaction([actionTxGroup]);
-    console.log(signedTx);
-    const { txId } = await client.sendRawTransaction(signedTx).do();
-    const result = await waitForConfirmation(client, txId, 2);
-    console.log(`Success`);
-  } catch (e) {
-    console.error(`There was an error calling the counter app: ${e}`);
+      const signedTx = await peraWallet.signTransaction([actionTxGroup]);
+      console.log(signedTx);
+      const { txId } = await client.sendRawTransaction(signedTx).do();
+      const result = await waitForConfirmation(client, txId, 2);
+      console.log(`Success`);
+    } catch (e) {
+      console.error(`There was an error calling the counter app: ${e}`);
+    }
   }
-}
   const register = () => {
     console.log(userAccount)
-    if(userAccount === null){
+    if (userAccount === null) {
       // alert("Connect your wallet")
       setAlert(true)
-    }else{
-      Optin(userAccount[0],CONSTANTS.APP_ID)
+    } else {
+      Optin(userAccount[0], CONSTANTS.APP_ID)
     }
 
   }
-  const handleClose = () =>{
+  const handleClose = () => {
     setAlert(false)
   }
-  return(
+  return (
     <div style={{ backgroundColor: '#E9E9E9' }}>
-      <Container style={{marginTop: '24px'}}>
+      <Container style={{ marginTop: '24px' }}>
         <Row>
           <Col>
-            <img src={logo}  alt='vote'/>
+            <img src={logo} alt='vote' />
           </Col>
           <Col>
             <div>
-            {/* <label for="StartDate">Start Date</label> */}
-            {/* <input type="date" id="start" name="startDate" value={startDate} onChange={(e) => setStartDate(e.currentTarget.value)}/> */}
-            {/* <h5>Reg Time</h5> */}
+              {/* <label for="StartDate">Start Date</label> */}
+              {/* <input type="date" id="start" name="startDate" value={startDate} onChange={(e) => setStartDate(e.currentTarget.value)}/> */}
+              {/* <h5>Reg Time</h5> */}
             </div>
             <div>
-            {/* <label for="EndDate">End Date</label> */}
-            {/* <h5>Vote Time</h5> */}
-            {/* <input type="date" id="end" name="endDate" value={endDate} onChange={(e) => setEndDate(e.currentTarget.value)}/> */}
+              {/* <label for="EndDate">End Date</label> */}
+              {/* <h5>Vote Time</h5> */}
+              {/* <input type="date" id="end" name="endDate" value={endDate} onChange={(e) => setEndDate(e.currentTarget.value)}/> */}
             </div>
           </Col>
-          <Col md='4' style={{display: 'inline'}}> 
-          {/* <h4>Voting Ends in </h4> */}
-          <Button style={{backgroundColor: 'black'}} onClick={() => register()}>Register</Button>
-          {/* <h4 id='demo' style={{color: 'red'}}></h4> */}
-          <MessageAlert show={alert} close={() => handleClose()} variant={"danger"} title="Connect Wallet" message= "Please connect your wallet"/>
+          <Col md='4' style={{ display: 'inline' }}>
+            {/* <h4>Voting Ends in </h4> */}
+            <Button style={{ backgroundColor: 'black' }} onClick={() => register()}>Register</Button>
+            {/* <h4 id='demo' style={{color: 'red'}}></h4> */}
+            <MessageAlert show={alert} close={() => handleClose()} variant={"danger"} title="Connect Wallet" message="Please connect your wallet" />
           </Col>
           <Col md='auto'>
-            <Button style={{backgroundColor: '#F28FA9', borderColor: '#F28FA9'}} onClick={connectAlgoSigner}>Connect Wallet</Button>
+            {!userAccount ? <Button style={{ backgroundColor: '#F28FA9', borderColor: '#F28FA9' }} onClick={connectAlgoSigner}>Connect Wallet</Button> : <Button style={{ backgroundColor: '#F28FA9', borderColor: '#F28FA9' }} onClick={handleDisconnectWalletClick}>Disconnect</Button>}
           </Col>
         </Row>
       </Container>
